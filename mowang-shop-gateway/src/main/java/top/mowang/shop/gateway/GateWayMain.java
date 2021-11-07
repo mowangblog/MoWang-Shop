93b2435cf04e08e4f83b923fb438cf7780c654b8
Xuan Li <mowangblog@qq.com>
2021-11-07 16:25:40 +0800

    添加SpringCloud Alibaba相关组件

---
+++ mowang-shop-gateway/src/main/java/top/mowang/shop/gateway/GateWayMain.java
@@ -0,0 +1,22 @@
+package top.mowang.shop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * MoWang-Shop
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/11/07 16:09
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GateWayMain {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain.class,args);
+    }
+}
